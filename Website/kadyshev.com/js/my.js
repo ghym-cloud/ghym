2a7b1353e3f6a70c684ade7c0d8642ce8423256d
maxkadyshev@gmail.com <maxkadyshev@gmail.com>
2023-10-28 17:40:45 +0400

    web sync

--- Website/kadyshev.com/js/my.js
+++ Website/kadyshev.com/js/my.js
@@ -5,19 +5,59 @@ window.onload = function() {
  performanceDisplay.innerText = loadTime / 1000;
 }
 var modal = document.getElementById("myModal");
-var span = document.getElementsByClassName("close")[0];
-function myBtn(img) {
- document.getElementById("myImg").src = img;
- modal.style.display = "block";
-}
-function mySlide(img) {
- document.getElementById("myModal").style.display = "block";
-}
-span.onclick = function() {
- modal.style.display = "none";
-}
+var modal2 = document.getElementById("myModal2");
+var modal3 = document.getElementById("myModal3");
+var modal4 = document.getElementById("myModal4");
+var modal5 = document.getElementById("myModal5");
 window.onclick = function(event) {
  if (event.target == modal) {
   modal.style.display = "none";
  }
+ if (event.target == modal2) {
+  modal2.style.display = "none";
+ }
+ if (event.target == modal3) {
+  modal3.style.display = "none";
+ }
+ if (event.target == modal4) {
+  modal4.style.display = "none";
+ }
+ if (event.target == modal5) {
+  modal5.style.display = "none";
+ }
+}
+let slideIndex = 1;
+function plusSlides(element,n) {
+  showSlides(element.title,slideIndex += n);
+}
+function currentSlide(element,n) {
+  showSlides(element.title,slideIndex = n);
+}
+function showSlides(w,n) {
+  let i;
+  let sl;
+  let slides = document.getElementsByClassName("mySlides");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  switch(w) {
+   case "cert":
+    modal.style.display = "block";
+    break;
+   case "restaurants":
+    modal2.style.display = "block";
+    break;
+   case "rese":
+    modal3.style.display = "block";
+    break;
+   case "carecarrier":
+    modal4.style.display = "block";
+    break;
+   case "lunchbox":
+    modal5.style.display = "block";
+    break;
+  }
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  slides[slideIndex-1].style.display = "block";
 }
